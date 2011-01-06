e4ccddb1ceb8a1a01d6c1584e33ac3aca7686ac8
Brian Temple <brian@beast.local>
2011-01-05 21:42:05 -0700

    Show 401 errors instead of redirecting when unauthorized.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   def render_404
     render(:file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404)
   end
+  
+  def render_401
+    # Devise intercepts 401 responses until 1.2.rc.  Status should be 401, not 404
+    render(:file => "#{RAILS_ROOT}/public/401.html", :layout => false, :status => 404)
+  end
+  
 end
